487a12a0b60f2e4627ab0d9569a09b78d323acad
Blaise <mynameisblaiseflorendo@gmail.com>
2022-11-18 16:20:56 +0800

    data can now read

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,176 @@
-import React, { useEffect, useState } from "react";
-// import logo from "./logo.svg";
+import React, { Fragment, useEffect, useState } from "react";
 import "./App.css";
+import { Products } from "./interfaces/products.interface";
+import { AddData, GetData } from "./DataProperties";
+import { Users } from "./interfaces/users.interface";
+import { RowReadOnly } from "./helper/ReadOnlyRow";
+import { WriteOnlyRow } from "./helper/WriteOnlyRow";
 
-export const App = (props: any) => {
-  const [data, setData] = useState([]]);
-  const URL = "https://dummyjson.com/products/categories";
+export const App = () => {
+  // Products
+  // const [prodData, setProdData] = useState<Products[]>([]);
+  // const [addData, setAddData] = useState<Products[]>([]);
+  // const [removeData, setRemoveData] = useState<Products[]>([]);
+
+  // Users
+  const [userData, setUserData] = useState<Users[]>([]);
+  const [addUserData, setAddUserData] = useState<Users[]>([]);
 
   useEffect(() => {
-    fetchData();
+    const startFetching = async () => {
+      const result = await GetData();
+      if (!ignore) setUserData(result);
+    };
+
+    // const addData = async () => {
+    //   const newResult = await AddData();
+    //   // if (!ignore) setProdDataValue(newResult);
+    // };
+
+    let ignore = false;
+    startFetching();
+    return () => {
+      ignore = true;
+    };
   }, []);
 
-  const fetchData = async () => {
-    // for (let i = 0; i < )
-    // data.datas.map(item => ())
-    const response = await fetch(`https://dummyjson.com/products/categories`);
-    const newData = await response.json();
-    setData(newData);
-    console.log(newData);
+  const handleAddUserChange = (event: any) => {
+    event.preventDefault();
+
+    const dataName = event.target.getAttribute("name");
+    const dataValue = event.target.value;
+    const newData = { ...addUserData };
+    newData[dataName] = dataValue;
+
+    setAddUserData(newData);
   };
 
-  // return (
-  //   <div>
-  //     {datas.map((data, index) => {
-  //       return (
-  //         <div key={index}>
-  //           <h2>id: {data.products}</h2>
-  //         </div>
-  //       );
-  //     })}
-  //   </div>
-  // );
-  return null;
-  // return (
-  //   <ul>
-  //     {data.datas.map(item => (
-  //       <li key={item.id}>
-  //         <a href={item.url}>{item.title}</a>
-  //       </li>
-  //     ))}
-  //   </ul>
-  // );
-};
+  const handleAddUserSubmit = (event: any) => {
+    event.preventDefault();
+    const newUserData = {
+      // id: addUserData.id,
+      // firstName: addUserData.firstName,
 
-// export default App;
+    }
+  }
+
+  // const removeDataHandler = (event: number) => {
+  //   const removeDataId = event.target.getAttribute("data-remove");
+
+  //   const newData = { ...removeData };
+  //   setRemoveData(newData);
+  // };
+
+  return (
+    <div>
+      {/* <table>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Title</th>
+            <th>Description</th>
+            <th>Price</th>
+            <th>Discount Percentage</th>
+            <th>Rating</th>
+            <th>Stock</th>
+            <th>Brand</th>
+            <th>Category</th>
+            <th>Thumbnail</th>
+          </tr>
+        </thead>
+        <tbody>
+          {prodData.map((data, i) => (
+            <tr key={i}>
+              <td>
+                <button onClick={removeDataHandler} data-remove={data.id}>X</button>
+                {data.id}
+              </td>
+              <td>{data.title}</td>
+              <td>{data.description}</td>
+              <td>{data.price}</td>
+              <td>{data.discountPercentage}</td>
+              <td>{data.rating}</td>
+              <td>{data.stock}</td>
+              <td>{data.brand}</td>
+              <td>{data.category}</td>
+              <td>
+                <img src={data.thumbnail} height="100" width="100" />
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table> */}
+
+      <form>
+        <table>
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>First Name</th>
+              <th>Last Name</th>
+              <th>Age</th>
+              <th>Gender</th>
+              <th>Email</th>
+              <th>Phone</th>
+            </tr>
+          </thead>
+          <tbody>
+            {userData.map((data, i) => (
+              // <Fragment>
+              // <WriteOnlyRow />
+              <RowReadOnly key={i} user={data}></RowReadOnly>
+              // </Fragment>
+            ))}
+          </tbody>
+        </table>
+      </form>
+
+      <h2>Add Contact</h2>
+      <form>
+        <input
+          type="number"
+          // required="required"
+          placeholder="Enter id"
+          name="id"
+        ></input>
+        <input
+          type="text"
+          // required="required"
+          placeholder="Enter first name"
+          name="firstName"
+        ></input>
+        <input
+          type="text"
+          // required="required"
+          placeholder="Enter last name"
+          name="lastName"
+        ></input>
+        <input
+          type="number"
+          // required="required"
+          placeholder="Enter age"
+          name="age"
+        ></input>
+        <input
+          type="text"
+          // required="required"
+          placeholder="Enter gender"
+          name="gender"
+        ></input>
+        <input
+          type="email"
+          // required="required"
+          placeholder="Enter email"
+          name="email"
+        ></input>
+        <input
+          type="text"
+          // required="required"
+          placeholder="Enter contact number"
+          name="phone"
+        ></input>
+        <button type="submit">Add</button>
+      </form>
+    </div>
+  );
+};
